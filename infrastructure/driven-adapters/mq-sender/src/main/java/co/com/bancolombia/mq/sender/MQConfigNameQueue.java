934c2a8913d0b700ef67bf349c8374f9e4d3b646
maocq <johncarm@bancolombia.com.co>
2023-08-03 18:52:14 -0500

    ConnectionFactory

--- infrastructure/driven-adapters/mq-sender/src/main/java/co/com/bancolombia/mq/sender/MQConfigNameQueue.java
+++ infrastructure/driven-adapters/mq-sender/src/main/java/co/com/bancolombia/mq/sender/MQConfigNameQueue.java
@@ -14,4 +14,32 @@ public class MQConfigNameQueue {
     public Destination destinationQueue(@Value("${commons.jms.input-queue}") String queue) throws JMSException {
         return new MQQueue(queue);
     }
+
+    /*
+    @Bean
+    @Profile("!local")
+    public ConnectionFactory getMyConnectionFactory(MQConfigurationProperties properties)
+            throws MalformedURLException, JMSException {
+        URL ccdtUrl = new File("/Users/johncarm/mao/java/mq-connection/d2b-mq-ccdt.json").toURI().toURL();
+        SSLSocketFactory sslSocketFactory = SSLContextUtils.buildSSLContextFromJks(
+                        "/Users/johncarm/mao/java/mq-connection/d2b-mq-credentials.jks", "axhNLiaGrT5SknK5")
+                .getSocketFactory();
+
+        System.setProperty("com.ibm.mq.cfg.useIBMCipherMappings", "false");
+        JmsFactoryFactory ff = JmsFactoryFactory.getInstance(JAKARTA_WMQ_PROVIDER);
+        MQConnectionFactory mqConnection = (MQConnectionFactory) ff.createConnectionFactory();
+        mqConnection.setIntProperty(WMQ_CONNECTION_MODE, WMQ_CM_CLIENT);
+        mqConnection.setTransportType(WMQ_CM_CLIENT);
+        mqConnection.setClientReconnectOptions(WMQ_CLIENT_RECONNECT);
+
+        mqConnection.setStringProperty(WMQ_QUEUE_MANAGER, "ALMSGRP");
+        mqConnection.setStringProperty(USERID, "cnxqqalm");
+        mqConnection.setCCDTURL(ccdtUrl);
+        mqConnection.setSSLSocketFactory(sslSocketFactory);
+        if (properties.getTempModel() != null) {
+            mqConnection.setStringProperty(WMQ_TEMPORARY_MODEL, properties.getTempModel());
+        }
+        return mqConnection;
+    }
+     */
 }
