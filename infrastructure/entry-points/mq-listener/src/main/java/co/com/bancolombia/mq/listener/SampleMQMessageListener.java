0ec035212c4d5fa229888e926c3d3fe46537a2ee
maocq <carmonaesc@gmail.com>
2024-06-13 10:31:11 -0500

    Dto entry point

--- infrastructure/entry-points/mq-listener/src/main/java/co/com/bancolombia/mq/listener/SampleMQMessageListener.java
+++ infrastructure/entry-points/mq-listener/src/main/java/co/com/bancolombia/mq/listener/SampleMQMessageListener.java
@@ -1,10 +1,15 @@
 package co.com.bancolombia.mq.listener;
 
 import co.com.bancolombia.commons.jms.mq.MQListener;
+import co.com.bancolombia.mq.listener.dtos.AccountRequestDto;
 import co.com.bancolombia.usecase.createaccount.CreateAccountUseCase;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Timer;
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.beanio.Marshaller;
+import org.beanio.StreamFactory;
+import org.beanio.Unmarshaller;
+import org.beanio.builder.StreamBuilder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -13,24 +18,33 @@ import jakarta.jms.Message;
 import jakarta.jms.TextMessage;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
-@AllArgsConstructor
 public class SampleMQMessageListener {
-    private final Timer timer = Metrics.timer("mq_receive_message", "operation", "my-operation"); // TODO: Change operation name
+    private final Timer timer = Metrics.timer("mq_receive_message", "operation", "my-operation");
 
     private final CreateAccountUseCase createAccountUseCase;
+    private final Marshaller accountMarshaller;
+    private final Unmarshaller accountUnmarshaller;
 
-    // For fixed queues
+    public SampleMQMessageListener(CreateAccountUseCase createAccountUseCase) {
+        var streamFactory = StreamFactory.newInstance();
+        streamFactory.define(new StreamBuilder("accountRequestDto").format("fixedlength")
+                .addRecord(AccountRequestDto.class));
+
+        this.accountMarshaller = streamFactory.createMarshaller("accountRequestDto");
+        this.accountUnmarshaller = streamFactory.createUnmarshaller("accountRequestDto");
+        this.createAccountUseCase = createAccountUseCase;
+    }
+
     @MQListener("${commons.jms.input-queue}")
     public Mono<Void> process(Message message) throws JMSException {
         timer.record(System.currentTimeMillis() - message.getJMSTimestamp(), TimeUnit.MILLISECONDS);
         String text = ((TextMessage) message).getText();
 
-        return createAccountUseCase.create(text)
-                .then();
-        /*
-        System.out.println("Message received: " + text);
-        return Mono.empty();
-         */
+        var dto = (AccountRequestDto) accountUnmarshaller.unmarshal(text);
+        //String plainData = accountMarshaller.marshal(dto).toString();
+        log.info("Received message: {}", dto);
+        return createAccountUseCase.create(dto.getName()).then();
     }
 }
---
+++ infrastructure/entry-points/mq-listener/src/main/java/co/com/bancolombia/mq/listener/dtos/AccountRequestDto.java
@@ -0,0 +1,23 @@
+package co.com.bancolombia.mq.listener.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.beanio.annotation.Field;
+import org.beanio.annotation.Record;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Record(name = "AccountRequestDto")
+public class AccountRequestDto {
+
+    @Field(name = "header", length = 20)
+    private String header;
+    @Field(name = "name", length = 8)
+    private String name;
+    @Field(name = "balance", length = 10)
+    private int balance;
+}
