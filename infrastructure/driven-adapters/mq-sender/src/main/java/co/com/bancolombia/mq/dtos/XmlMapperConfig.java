30036af6c93ef24723e778b4d0f493665a65b23f
maocq <carmonaesc@gmail.com>
2024-06-19 11:03:22 -0500

    Fix XmlMapper

--- infrastructure/driven-adapters/mq-sender/src/main/java/co/com/bancolombia/mq/dtos/XmlMapperConfig.java
+++ infrastructure/driven-adapters/mq-sender/src/main/java/co/com/bancolombia/mq/dtos/XmlMapperConfig.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Configuration;
 
 import static com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator.Feature.WRITE_XML_DECLARATION;
 
-@Configuration
+//@Configuration
 public class XmlMapperConfig {
 
-    @Bean
+    /**
+     * Para evitar coflictos con el ObjectMapper de Spring se recomienda instanciar el XmlMapper en el constructor
+     * de la clase que lo necesita usar o hacer uso de un wrapper. Ver (SampleMQReqReply.java)
+     */
+    //@Bean
     public XmlMapper xmlMapper() {
         var xmlMapper = new XmlMapper();
         xmlMapper.configure(WRITE_XML_DECLARATION, false);
--- infrastructure/driven-adapters/mq-sender/src/main/java/co/com/bancolombia/mq/reqreply/SampleMQReqReply.java
+++ infrastructure/driven-adapters/mq-sender/src/main/java/co/com/bancolombia/mq/reqreply/SampleMQReqReply.java
@@ -5,7 +5,6 @@ import co.com.bancolombia.model.account.Account;
 import co.com.bancolombia.model.account.gateways.AccountService;
 import co.com.bancolombia.mq.dtos.AccountXmlDto;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -13,13 +12,23 @@ import reactor.core.publisher.Mono;
 import jakarta.jms.Message;
 import jakarta.jms.TextMessage;
 
+import static com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator.Feature.WRITE_XML_DECLARATION;
+
 @Component
-@AllArgsConstructor
 @EnableMQGateway(scanBasePackages = "co.com.bancolombia")
 public class SampleMQReqReply implements AccountService {
 
     private final ReqReplyGateway sender;
     private final XmlMapper xmlMapper;
+
+    public SampleMQReqReply(ReqReplyGateway sender) {
+        var xmlMapper = new XmlMapper();
+        xmlMapper.configure(WRITE_XML_DECLARATION, false);
+        //xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        this.sender = sender;
+        this.xmlMapper = xmlMapper;
+    }
 
     @Override
     public Mono<Account> createAccount(Account account) {
